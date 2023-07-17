1256983fdbaa0cf123f7f958b32e78d5b9be0e54
Samuel <absurd.turtl3@gmail.com>
2023-07-17 22:05:59 +0200

    fixed particles; added app button

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueParticles from 'particles.vue3'
+import Particles from 'particles.vue3'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(VueParticles)
+app.use(Particles)
 
 app.mount('#app')
